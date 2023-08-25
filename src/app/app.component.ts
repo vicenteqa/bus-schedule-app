83950be45dc2848c165c0c73cb99580e0b04f9b3
vicente ruiz <virucu@gmail.com>
2023-08-25 13:15:08 +0200

    add routes lesson 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   template: `
     <main>
       <header class="brand-name">
@@ -16,7 +17,7 @@ import { HomeComponent } from './home/home.component';
         />
       </header>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
     </main>
   `,
