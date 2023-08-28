54c8f8d05665f7b7ebb12df13e57af5dabd1377f
vicente ruiz <virucu@gmail.com>
2023-08-28 15:14:14 +0200

    lesson 12

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -112,6 +112,12 @@ export class HousingService {
 
   constructor() {}
 
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`
+    );
+  }
+
   getAllHousingLocations(): HousingLocation[] {
     return this.housingLocationList;
   }
